ad0e8c4b8a12e9cb35d368832672e29090f19dc1
kashhiiiffff <kashifrather123@gmail.com>
2024-11-13 20:45:08 +0530

    Add files via upload

---
+++ bloodbank (1).sql
@@ -0,0 +1,230 @@
+-- phpMyAdmin SQL Dump
+-- version 5.2.1
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Generation Time: Nov 13, 2024 at 04:14 PM
+-- Server version: 10.4.32-MariaDB
+-- PHP Version: 8.0.30
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `bloodbank`
+--
+
+DELIMITER $$
+--
+-- Procedures
+--
+CREATE DEFINER=`root`@`localhost` PROCEDURE `main` (IN `no` INT, IN `value` VARCHAR(20))   BEGIN
+    SELECT 
+        blood_type,
+        COUNT(blood_type) AS count1
+    FROM 
+        blood b1
+    WHERE 
+        blood_type = value
+        AND EXISTS (
+            SELECT 
+                1
+            FROM 
+                blood_bank b2
+            WHERE 
+                b2.blood_bank_id = no
+                AND b1.blood_bank_id = b2.blood_bank_id
+        )
+    GROUP BY 
+        blood_type;
+END$$
+
+DELIMITER ;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `blood`
+--
+
+CREATE TABLE `blood` (
+  `blood_type` varchar(20) DEFAULT NULL,
+  `donor_id` int(11) NOT NULL,
+  `blood_bank_id` int(11) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `blood_bank`
+--
+
+CREATE TABLE `blood_bank` (
+  `blood_bank_id` int(11) NOT NULL,
+  `blood_bank_name` varchar(50) DEFAULT NULL,
+  `baddress` varchar(30) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `blood_delivery`
+--
+
+CREATE TABLE `blood_delivery` (
+  `blood_bank_id` int(11) NOT NULL,
+  `patient_id` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `doctor`
+--
+
+CREATE TABLE `doctor` (
+  `doctor_id` int(11) NOT NULL,
+  `doctor_name` varchar(20) DEFAULT NULL,
+  `doc_add` varchar(50) DEFAULT NULL,
+  `doc_phno` bigint(20) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `donor`
+--
+
+CREATE TABLE `donor` (
+  `donor_id` int(11) NOT NULL,
+  `donor_name` varchar(20) DEFAULT NULL,
+  `phone_no` bigint(20) DEFAULT NULL,
+  `dob` date DEFAULT NULL,
+  `gender` char(1) DEFAULT NULL,
+  `address` varchar(30) DEFAULT NULL,
+  `weight` int(11) DEFAULT NULL,
+  `blood_pressure` int(11) DEFAULT NULL,
+  `iron_content` int(11) DEFAULT NULL,
+  `doctor_id` int(11) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `patient`
+--
+
+CREATE TABLE `patient` (
+  `patient_id` int(11) NOT NULL,
+  `patient_name` varchar(20) DEFAULT NULL,
+  `p_phno` bigint(20) DEFAULT NULL,
+  `h_add` varchar(50) DEFAULT NULL,
+  `p_add` varchar(50) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `blood`
+--
+ALTER TABLE `blood`
+  ADD PRIMARY KEY (`donor_id`),
+  ADD KEY `blood_bank_id` (`blood_bank_id`);
+
+--
+-- Indexes for table `blood_bank`
+--
+ALTER TABLE `blood_bank`
+  ADD PRIMARY KEY (`blood_bank_id`);
+
+--
+-- Indexes for table `blood_delivery`
+--
+ALTER TABLE `blood_delivery`
+  ADD PRIMARY KEY (`blood_bank_id`,`patient_id`),
+  ADD KEY `patient_id` (`patient_id`);
+
+--
+-- Indexes for table `doctor`
+--
+ALTER TABLE `doctor`
+  ADD PRIMARY KEY (`doctor_id`);
+
+--
+-- Indexes for table `donor`
+--
+ALTER TABLE `donor`
+  ADD PRIMARY KEY (`donor_id`),
+  ADD KEY `fk1` (`doctor_id`);
+
+--
+-- Indexes for table `patient`
+--
+ALTER TABLE `patient`
+  ADD PRIMARY KEY (`patient_id`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `blood_bank`
+--
+ALTER TABLE `blood_bank`
+  MODIFY `blood_bank_id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `doctor`
+--
+ALTER TABLE `doctor`
+  MODIFY `doctor_id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `donor`
+--
+ALTER TABLE `donor`
+  MODIFY `donor_id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `patient`
+--
+ALTER TABLE `patient`
+  MODIFY `patient_id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- Constraints for dumped tables
+--
+
+--
+-- Constraints for table `blood`
+--
+ALTER TABLE `blood`
+  ADD CONSTRAINT `blood_ibfk_1` FOREIGN KEY (`donor_id`) REFERENCES `donor` (`donor_id`),
+  ADD CONSTRAINT `blood_ibfk_2` FOREIGN KEY (`blood_bank_id`) REFERENCES `blood_bank` (`blood_bank_id`);
+
+--
+-- Constraints for table `blood_delivery`
+--
+ALTER TABLE `blood_delivery`
+  ADD CONSTRAINT `blood_delivery_ibfk_1` FOREIGN KEY (`blood_bank_id`) REFERENCES `blood_bank` (`blood_bank_id`),
+  ADD CONSTRAINT `blood_delivery_ibfk_2` FOREIGN KEY (`patient_id`) REFERENCES `patient` (`patient_id`);
+
+--
+-- Constraints for table `donor`
+--
+ALTER TABLE `donor`
+  ADD CONSTRAINT `fk1` FOREIGN KEY (`doctor_id`) REFERENCES `doctor` (`doctor_id`);
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
